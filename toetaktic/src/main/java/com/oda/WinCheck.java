40a3e2c534ede35bb9fdff3d28e4c3e95519b67e
Halil Kaplan <h.kaplan@optadata-gruppe.de>
2023-12-12 09:53:32 +0100

    added winning logic (madness)

--- toetaktic/src/main/java/com/oda/WinCheck.java
+++ toetaktic/src/main/java/com/oda/WinCheck.java
@@ -4,6 +4,19 @@ import com.oda.GUI.ClickFieldEvent;
 import com.oda.GUI.GUI;
 
 public class WinCheck {
+    static byte winCondition;
+    static boolean[] won;
+    static int gridSize = GUI.getBoard().length;
+
+    public static void winConditionCheck(int x , int y, int userID){
+        byte[][] board = GUI.getBoard();
+        if (board[x+1][y+1] < gridSize){
+            if(board[x][y] == board[x+1][y+1] && board[x][y] == board[x+2][y+2]){
+                won[userID] = true;
+            }
+
+        }
+    }
     public static void test(){
         GUI.addListener(new ClickFieldEvent() {
             @Override
